96869e2926f4e9597a621fa26f008790740001d8
Kajal Das <kajaldas60@gmail.com>
2025-10-26 12:40:38 +0530

    row wise sum of 2d matrix

---
+++ sum_2d.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class sum_2d {
+    public static void main(String[] args) {
+        // to take the input from the system
+        Scanner s = new Scanner(System.in);
+        // taken a variable to get the value from the user
+        System.out.print("Enter the number of rows:");
+        int rows = s.nextInt();
+        System.out.print("Enter the number of columns:");
+        int cols = s.nextInt();
+
+        int[][] a = new int[rows][cols];
+
+        // input
+        System.out.println("Enter the elements:");
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                a[i][j] = s.nextInt();
+            }
+        }
+
+        //output
+        System.out.println("sum:");
+        
+        for(int i=0;i<rows;i++){
+            int sum =0;
+            for(int j=0;j<cols;j++){
+                sum += a[i][j];
+            }
+             System.out.println("Sum of the row"+ (i+1) +": "+sum);
+        }
+
+    }
+}
